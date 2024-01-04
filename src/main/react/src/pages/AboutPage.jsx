51d05184a7aa62f291b855c54635c81cce0248ca
lastofyears21 <lastofyears21@gmail.com>
2024-01-04 09:03:36 +0900

    24/01/04 정벼리 하늘씨 버전에서 누락된 버전 import, 업데이트 , 빌드 실행 확인 완료

---
+++ src/main/react/src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import AboutMain from "../components/aboutPage/AboutMain";
+import { Center } from "../components/PublicStyle";
+
+
+
+
+const AboutPage = () => {
+  return (
+    <Center>
+        <AboutMain/>
+    </Center>
+
+  );
+};
+
+export default AboutPage;
